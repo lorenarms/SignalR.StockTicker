77200f59f6675ca5ceecb17513829977f16216a0
Lawrence <lorenzo.joseph.dominguez@gmail.com>
2023-09-20 19:56:34 -0700

    next commit

---
+++ ShopOnline.API/Repositories/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using ShopOnline.API.Entities;
+using ShopOnline.API.Repositories.Contracts;
+
+namespace ShopOnline.API.Repositories
+{
+	public class ProductRepository : IProductRepository
+	{
+		public Task<IEnumerable<Product>> GetItems()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<ProductCategory>> GetCategories()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Product> GetItme(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<ProductCategory> GetCategory(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
